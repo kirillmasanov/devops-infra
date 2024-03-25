2031ad7be86f3ec0f460d264201033d9439d465c
kirill.masanov <kirill_masanov@mail.ru>
2024-03-25 08:07:59 +0000

    change

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,17 @@
 terraform {
-        required_providers {
+    required_providers {
         vcd = {
-        source = "vmware/vcd"
+            source = "vmware/vcd"
         }
     }
     required_version = ">= 0.13"
+}
+
+provider "vcd" {
+    auth_type = "integrated" #Тип авторизации
+    max_retry_timeout = 10 #Максимальное число попыток соединения
+    user = var.connect["user"] #Имя пользователя
+    password = var.connect["password"] #Пароль пользователя
+    org = var.connect["organization"] #Название организации
+    url = var.connect["url_connect"] #Адрес, на который будут посылаться API-запросы
 }
