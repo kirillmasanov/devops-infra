aeca81ec668947a68c8155867e44c6ccd20380aa
kirill.masanov <kirill_masanov@mail.ru>
2024-03-25 14:22:26 +0000

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ terraform {
     required_providers {
         vcd = {
             source = "vmware/vcd"
+            version = "3.9.0"
         }
     }
     required_version = ">= 0.13"
@@ -15,6 +16,7 @@ provider "vcd" {
     org = var.vcd_org_org
     vdc = var.vcd_org_vdc
     url = var.vcd_org_url
+    allow_unverified_ssl = var.vcd_org_allow_unverified_ssl
 }
 
 resource "vcd_network_routed" "terraform-net" {
